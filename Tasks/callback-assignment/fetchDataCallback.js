4b31110334f8a26cd3ba731c61a2d5af26add381
el-hussain <hussainisuleimanahmad@gmail.com>
2025-08-09 22:54:06 +0100

    call back function

---
+++ Tasks/callback-assignment/fetchDataCallback.js
@@ -0,0 +1,21 @@
+function fetchDataWithCallback(callback) {
+    console.log("Fetching data...");
+
+    setTimeout(function() {
+        try {
+            let error = true;
+
+            if (error) {
+                throw new Error("Fetch failed");
+            }
+
+            callback("Data fetched");
+        } catch (err) {
+            console.log("Error: " + err.message);
+        }
+    }, 2000); 
+}
+
+fetchDataWithCallback(function(message) {
+    console.log(message);
+});
